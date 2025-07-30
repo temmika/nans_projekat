fcb265a9d83a4a35f8c5a9dcb3f9ee46d1d990fd
temmika <temmika555@gmail.com>
2025-07-30 16:27:59 +0200

    Ciscenje

--- predict_svm_hog.py
+++ predict_svm_hog.py
@@ -1,6 +1,8 @@
 from svm_hog.svm_hog_model import predict_image
 
 if __name__ == "__main__":
-    img_path = r"data/test/carambola.jpeg"
+    img_path = r"data/test/test5.png"
     predikcija = predict_image(img_path)
     print(f"Predikcija za sliku: {predikcija}")
+
+    
